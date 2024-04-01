f25752367e476fa3b726ac39f46e84faef8ba010
Jocelyn409 <jocelynlahaise@gmail.com>
2024-04-01 00:30:08 -0400

    some updatesss

--- commands/increment.js
+++ commands/increment.js
@@ -13,13 +13,16 @@ module.exports = {
         if(jsonData.counterName !== null) {
             // Update total
             jsonData.runningTotal = jsonData.runningTotal + 1;
-            fileSync.writeFileSync("./counter.json", JSON.stringify(jsonData));
             console.log("Counter total is now: " + jsonData.runningTotal);
 
-            // Relay information
+            // Update individual score
             const user = interaction.options.getUser('user') || interaction.user;
             const member = await interaction.guild.members.fetch(user.id);
 
+            jsonData.userScores.member = 1;
+            fileSync.writeFileSync("./counter.json", JSON.stringify(jsonData));
+
+            // Relay information
             await interaction.reply(`${member} has added to the ${jsonData.counterName} Counter`);
             await interaction.followUp(`The total is now ${jsonData.runningTotal}!`);
         }
---
+++ commands/total.js
@@ -0,0 +1,21 @@
+const { SlashCommandBuilder } = require("discord.js");
+const fileSync = require("fs");
+
+module.exports = {
+
+    data: new SlashCommandBuilder()
+        .setName('total')
+        .setDescription('Show the total counter amount'),
+
+    async execute(interaction) {
+        let jsonData = JSON.parse(fileSync.readFileSync("./counter.json", 'utf-8'));
+
+        if(jsonData.counterName !== null) {
+            await interaction.reply(`Counter total: ${jsonData.runningTotal}`);
+        }
+        else {
+            await interaction.reply("You need to name the counter before viewing the total");
+        }
+    },
+
+};
